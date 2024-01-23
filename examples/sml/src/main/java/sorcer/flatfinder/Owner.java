4c070aa5e76d6c3bd9527cbf46d34e5f999a9069
DLPasternak <s21203@pjwstk.edu.pl>
2024-01-23 21:28:14 +0100

    Added User, Owner and Tenant entities

---
+++ examples/sml/src/main/java/sorcer/flatfinder/Owner.java
@@ -0,0 +1,9 @@
+package sorcer.flatfinder;
+
+import java.util.Date;
+
+public class Owner extends User {
+    public Owner(String firstName, String lastName, Date birthDate) {
+        super(firstName, lastName, birthDate);
+    }
+}
---
+++ examples/sml/src/main/java/sorcer/flatfinder/Tenant.java
@@ -0,0 +1,9 @@
+package sorcer.flatfinder;
+
+import java.util.Date;
+
+public class Tenant extends User {
+    public Tenant(String firstName, String lastName, Date birthDate) {
+        super(firstName, lastName, birthDate);
+    }
+}
