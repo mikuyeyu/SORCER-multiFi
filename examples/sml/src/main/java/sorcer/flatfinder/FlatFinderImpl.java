e72c85eab54aaa176300a079ed0597f04f497c86
Mikołaj Owczarek <s20899@pjwstk.edu.pl>
2024-01-23 21:06:55 +0100

    Added WIP flat finder implementation

---
+++ examples/sml/src/main/java/sorcer/flatfinder/FlatFinderImpl.java
@@ -0,0 +1,23 @@
+package sorcer.flatfinder;
+
+import sorcer.service.Context;
+import sorcer.service.ContextException;
+
+import java.rmi.RemoteException;
+
+public class FlatFinderImpl implements FlatFinder {
+    @Override
+    public Context findBest(Context context) throws RemoteException, ContextException {
+        return null;
+    }
+
+    @Override
+    public Context getDetails(Context context) throws RemoteException, ContextException {
+        return null;
+    }
+
+    @Override
+    public Context findNearest(Context context) throws RemoteException, ContextException {
+        return null;
+    }
+}
